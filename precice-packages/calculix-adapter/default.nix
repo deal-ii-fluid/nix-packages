8c434f409ec3348e6fbcff7b1020ea3ccd70d424
jiaqiwang969 <jiaqiwang969@gmail.com>
2025-01-04 23:36:56 -0800

    fix:->

--- precice-packages/calculix-adapter/default.nix
+++ precice-packages/calculix-adapter/default.nix
@@ -3,7 +3,7 @@
 , fetchFromGitHub
 , fetchzip
 , gcc
-, gfortran  # explicitly bring in the Fortran compiler if needed
+, gfortran
 , pkg-config
 , arpack
 , lapack
@@ -12,22 +12,24 @@
 , libyamlcpp
 , precice
 , openmpi
+, substituteAll
 }:
+
 let
   ccx_version = "2.20";
   ccx = fetchzip {
     urls = [
       "https://www.dhondt.de/ccx_2.20.src.tar.bz2"
+      # Mirror fallback
       "https://web.archive.org/web/20240302101853if_/https://www.dhondt.de/ccx_2.20.src.tar.bz2"
     ];
-    hash = "sha256-bCmG+rcQDJrcwDci/WOAgjfbhy1vxdD+wnwRlt/ovKo=";
+    sha256 = "sha256-bCmG+rcQDJrcwDci/WOAgjfbhy1vxdD+wnwRlt/ovKo=";
   };
 in
 stdenv.mkDerivation rec {
   pname = "calculix-adapter";
   version = "${ccx_version}.0";
 
-  # Pull the preCICE CalculiX adapter from GitHub
   src = fetchFromGitHub {
     owner = "precice";
     repo = pname;
@@ -35,10 +37,13 @@ stdenv.mkDerivation rec {
     hash = "sha256-zyJ9VOpmjyBeettPWA3bFZIfyJuvs5D1nMxRcP5ySRY=";
   };
 
-  # Both stdenv.cc and gfortran are used, plus openmpi for mpifort
+  # Tools needed at build-time only
   nativeBuildInputs = [
+    # The C/C++ compiler from stdenv
     stdenv.cc
+    # The Fortran compiler
     gfortran
+    # HPC libraries needed for linking
     pkg-config
     arpack
     lapack
@@ -49,19 +54,26 @@ stdenv.mkDerivation rec {
     openmpi
   ];
 
-  # Patch or override CC in the Makefile. This approach assumes the Makefile
-  # respects the 'CC' variable. If it’s truly hard-coded, use substituteInPlace.
+  # If you needed these at runtime, you'd put them in buildInputs or
+  # propagatedBuildInputs, but typically the final binary is self-contained.
+  buildInputs = [];
 
-  # If the Makefile actually respects CC/FC environment variables, you can do:
-   makeFlags = [
-     "CC=${stdenv.cc.cc}"
-     "FC=${gfortran}/bin/gfortran"
-   ];
+  # This is similar to patchShebangs or other approach from openmpi
+  postPatch = ''
+    # If the Makefile references /usr/local/bin/gcc or something else:
+    substituteInPlace Makefile --replace "/usr/local/bin/gcc" "${stdenv.cc.cc}"
+    # If it references /usr/local/bin/gfortran or something else:
+    substituteInPlace Makefile --replace "/usr/local/bin/gfortran" "${gfortran}/bin/gfortran"
 
+    # If you need to fix absolute references to mpifort in the Makefile:
+    substituteInPlace Makefile --replace "mpifort" "${openmpi}/bin/mpifort"
+  '';
+
+  # We can echo which compilers are actually being used, for debugging
   buildPhase = ''
-    echo "Using CC=${CC:-unset}, FC=${FC:-unset}"
-    mpifort --version
-    # Now run make. If the Makefile uses CC, it will have been patched.
+    echo "==== Debug: Using CC=${CC:-unset}, FC=${FC:-unset}, PATH=$PATH"
+    mpifort --version || true
+    # Now run "make -j" with the relevant flags
     make -j \
       CCX=${ccx}/ccx_2.20/src \
       SPOOLES_INCLUDE="-I${spooles}/include/spooles/" \
@@ -74,18 +86,15 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/{bin,lib}
-    cp bin/ccx_preCICE $out/bin
-    cp bin/ccx_2.20.a $out/lib
+    cp bin/ccx_preCICE $out/bin/
+    cp bin/ccx_2.20.a $out/lib/
   '';
 
-  meta = {
+  meta = with lib; {
     description = "preCICE-adapter for the CSM code CalculiX";
     homepage = "https://precice.org/adapter-calculix-overview.html";
-    license = with lib.licenses; [ gpl3 ];
-    maintainers = with lib.maintainers; [ conni2461 ];
-    mainProgram = "ccx_preCICE";
-    # CalculiX is mostly for UNIX-like systems. If you want it on Darwin, you can
-    # keep 'lib.platforms.all'. But typically it’s for Linux + maybe macOS/other unixes.
-    platforms = lib.platforms.unix;
+    license = licenses.gpl3;
+    maintainers = [ maintainers.conni2461 ];
+    platforms = platforms.unix;
   };
 }
