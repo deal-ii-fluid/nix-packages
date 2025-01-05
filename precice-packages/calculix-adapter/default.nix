2e9a8f24347ce46b352952b8baba59a3263e1db2
jiaqiwang969 <jiaqiwang969@gmail.com>
2025-01-04 23:09:36 -0800

    Fix: Use stdenv C compiler in CalculiX Makefile
    The Makefile for CalculiX was hardcoding the use of /usr/local/bin/gcc.
    Updated the Makefile to either replace this path with the compiler from
    stdenv or override the CC variable at make time, ensuring compatibility
    across non-NixOS platforms like macOS.

--- precice-packages/calculix-adapter/default.nix
+++ precice-packages/calculix-adapter/default.nix
@@ -1,17 +1,17 @@
-{
-  lib,
-  stdenv,
-  fetchFromGitHub,
-  fetchzip,
-  gcc,
-  pkg-config,
-  arpack,
-  lapack,
-  blas,
-  spooles,
-  libyamlcpp,
-  precice,
-  openmpi,
+{ lib
+, stdenv
+, fetchFromGitHub
+, fetchzip
+, gcc
+, gfortran  # explicitly bring in the Fortran compiler if needed
+, pkg-config
+, arpack
+, lapack
+, blas
+, spooles
+, libyamlcpp
+, precice
+, openmpi
 }:
 let
   ccx_version = "2.20";
@@ -27,6 +27,7 @@ stdenv.mkDerivation rec {
   pname = "calculix-adapter";
   version = "${ccx_version}.0";
 
+  # Pull the preCICE CalculiX adapter from GitHub
   src = fetchFromGitHub {
     owner = "precice";
     repo = pname;
@@ -34,8 +35,10 @@ stdenv.mkDerivation rec {
     hash = "sha256-zyJ9VOpmjyBeettPWA3bFZIfyJuvs5D1nMxRcP5ySRY=";
   };
 
+  # Both stdenv.cc and gfortran are used, plus openmpi for mpifort
   nativeBuildInputs = [
-    gcc
+    stdenv.cc
+    gfortran
     pkg-config
     arpack
     lapack
@@ -46,8 +49,19 @@ stdenv.mkDerivation rec {
     openmpi
   ];
 
+  # Patch or override CC in the Makefile. This approach assumes the Makefile
+  # respects the 'CC' variable. If it’s truly hard-coded, use substituteInPlace.
+
+  # If the Makefile actually respects CC/FC environment variables, you can do:
+   makeFlags = [
+     "CC=${stdenv.cc.cc}"
+     "FC=${gfortran}/bin/gfortran"
+   ];
+
   buildPhase = ''
+    echo "Using CC=${CC:-unset}, FC=${FC:-unset}"
     mpifort --version
+    # Now run make. If the Makefile uses CC, it will have been patched.
     make -j \
       CCX=${ccx}/ccx_2.20/src \
       SPOOLES_INCLUDE="-I${spooles}/include/spooles/" \
@@ -55,12 +69,11 @@ stdenv.mkDerivation rec {
       ARPACK_LIBS="$(${pkg-config}/bin/pkg-config --libs arpack lapack blas)" \
       YAML_INCLUDE="$(${pkg-config}/bin/pkg-config --cflags-only-I yaml-cpp)" \
       YAML_LIBS="$(${pkg-config}/bin/pkg-config --libs yaml-cpp)" \
-      ADDITIONAL_FFLAGS=-fallow-argument-mismatch
+      ADDITIONAL_FFLAGS="-fallow-argument-mismatch"
   '';
 
   installPhase = ''
     mkdir -p $out/{bin,lib}
-
     cp bin/ccx_preCICE $out/bin
     cp bin/ccx_2.20.a $out/lib
   '';
@@ -71,6 +84,8 @@ stdenv.mkDerivation rec {
     license = with lib.licenses; [ gpl3 ];
     maintainers = with lib.maintainers; [ conni2461 ];
     mainProgram = "ccx_preCICE";
+    # CalculiX is mostly for UNIX-like systems. If you want it on Darwin, you can
+    # keep 'lib.platforms.all'. But typically it’s for Linux + maybe macOS/other unixes.
     platforms = lib.platforms.unix;
   };
 }
